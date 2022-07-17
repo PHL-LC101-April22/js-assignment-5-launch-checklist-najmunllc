b94a2d415f2793294f125948c21330761d08d236
GJamillah <genevapughr@gmail.com>
2022-07-16 22:13:15 -0400

    added forms and planetReturn code

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,30 +1,69 @@
 // Write your helper functions here!
-require('isomorphic-fetch');
+('isomorphic-fetch');
 
-//function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
+function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
   
    //document.getElementById("missionTarget").innerHTML = 
-              //  <><h2>Mission Destination</h2>
-      // <ol>
-         //  <li>Name: ${json[index].name}</li>
-          // <li>Diameter: ${json[index].diameter}</li>
-           //<li>Star: ${star}</li>
-           //<li>Distance from Earth: ${json[index].distance}</li>
-           //<li>Number of Moons: ${json[index].moons}</li>
-      // </ol><img src="Moon_image.jpeg">
+  <><h2>Mission Destination</h2>
+       <ol>
+           <li>Name: ${json[index].name}</li>
+           <li>Diameter: ${json[index].diameter}</li>
+           <li>Star: ${star}</li>
+           <li>Distance from Earth: ${json[index].distance}</li>
+           <li>Number of Moons: ${json[index].moons}</li>
+       </ol>
+            <img src="Moon_image.jpeg">
 
     
 
               // function validateInput(testInput)
 
+        <script>
+                window.addEventListener("load", function() {
+                let form = document.querySelector("form");
+                form.addEventListener("submit", function(event) {
+                    alert("submit clicked");
+                 });
+            });
+        </script>
+
                //function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel)
+
+        <form method="POST" action="">
+            <label>Pilot <input type="text" name="pilotName"></label>
+            <label>Co-Pilot <input type="text" name="copilotName"></label>
+            <label>Fuel Level <input type="number" name="fuelLevel"></label>
+            <label>Cargo Mass <input type="number" name="cargoWeight"></label>
+               
+        </form>
+                <button id="update">Submit</button> 
 
               // async function myFetch() {let} planetsReturned;
 
               // planetsReturned = await fetch().then(function(response) );
 
-              // return planetsReturned;
+              planetsReturned = [];
+                let key = 0;
+                let index = 0;
+                for (json[key].index in json) {
+
+                 planet = [
+                    json[key].name,
+                    json[key].diameter,
+                    json[key].star,
+                    json[key].distance,
+                    json[key].moons,
+                    json[key].imageUrl
+                ]
+                     planetsReturned[key] = planet;
+
+                    return planet;
+                    key = (key + 1) % json.length;
+             }             
+    });
+              return planetsReturned;
+                
        // }
 
               // function pickPlanet(planets)
@@ -36,47 +75,4 @@ require('isomorphic-fetch');
               // module.exports.myFetch = myFetch;
           // </>
 
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Launch Checklist</title>
-        <link rel = "stylesheet" type = "text/css" href = "styles.css" />
-        <script src = "script.js"></script>
-    </head>
-    <body>
-        <h1>Launch Checklist Form</h1>
-        <div id="missionTarget">
-
-           //Fetch some planetary data
-           
-        </div>
-        <div id="launchForm">
-            <form>
-                <div style="display:flex; flex-direction: row; justify-content: center; align-items: center; margin: 8 0;">
-                    <label>Pilot Name <input type="text" name="pilotName" id="pilotName"/></label>
-                </div>
-                <div style="display:flex; flex-direction: row; justify-content: center; align-items: center; margin: 8 0;">
-                    <label>Co-pilot Name <input type="text" name="copilotName"/></label>
-                </div>
-                <div style="display:flex; flex-direction: row; justify-content: center; align-items: center; margin: 8 0;">
-                    <label>Fuel Level (gal) <input type="text" name="fuelLevel"/></label>
-                </div>
-                <div style="display:flex; flex-direction: row; justify-content: center; align-items: center; margin: 8 0;">
-                    <label>Cargo Weight (lbs) <input type="text" name="cargoWeight"/></label>
-                </div>
-                <button id="formSubmit">Submit</button>
-            </form>
-        </div>
-        <div id="launchStatusCheck">
-            <h2 id="launchStatus">Awaiting Information Before Launch</h2>
-            <div  id="faultyItems">
-                <ol>
-                    <li id="pilotStatus">Pilot Ready</li>
-                    <li id="copilotStatus">Co-pilot Ready</li>
-                    <li id="fuelStatus">Fuel level high enough for launch</li>
-                    <li id="cargoStatus">Cargo weight low enough for launch</li>
-                </ol>
-            </div>
-        </div>   
-    </body>
-</html>
+    
